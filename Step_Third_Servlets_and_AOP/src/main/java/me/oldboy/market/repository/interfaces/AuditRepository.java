3e69668a5c0ed79ed34850920578f5f11ffb1c73
Paul <100008373+JcoderPaul@users.noreply.github.com>
2026-01-19 08:56:33 +0500

    Step_Third_Servlets_and_AOP: add AuditRepository.java

---
+++ Step_Third_Servlets_and_AOP/src/main/java/me/oldboy/market/repository/interfaces/AuditRepository.java
@@ -0,0 +1,14 @@
+package me.oldboy.market.repository.interfaces;
+
+import me.oldboy.market.entity.Audit;
+import me.oldboy.market.repository.interfaces.crud.CrudDao;
+
+/**
+ * Data Access Object интерфейс для работы с сущностью {@link Audit}.
+ * Расширяет базовый CRUD функционал для операций с аудит-записями (фиксация действий пользователей).
+ *
+ * @see CrudDao
+ * @see Audit
+ */
+public interface AuditRepository extends CrudDao<Long, Audit> {
+}
