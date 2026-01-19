dd9daaa68843e7c3fd9d7061674ac5a6f0c4f39b
Paul <100008373+JcoderPaul@users.noreply.github.com>
2026-01-19 09:01:15 +0500

    Step_Third_Servlets_and_AOP: add UserService.java

---
+++ Step_Third_Servlets_and_AOP/src/main/java/me/oldboy/market/services/interfaces/UserService.java
@@ -0,0 +1,16 @@
+package me.oldboy.market.services.interfaces;
+
+import me.oldboy.market.entity.User;
+import me.oldboy.market.services.interfaces.crud.ReadOnlyService;
+
+/**
+ * Специализированный сервис для работы с пользователями.
+ * Предоставляет только операции чтения (поиска).
+ *
+ * @see ReadOnlyService
+ * @see User
+ */
+public interface UserService extends ReadOnlyService<Long, User> {
+    User getUserByEmail(String email);
+    boolean isEmailUnique(String email);
+}
