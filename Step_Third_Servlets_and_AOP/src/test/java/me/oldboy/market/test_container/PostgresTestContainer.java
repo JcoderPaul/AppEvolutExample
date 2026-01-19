7da656557884cd5a6026f9b090df4b8ffb365f7f
Paul <100008373+JcoderPaul@users.noreply.github.com>
2026-01-19 09:13:14 +0500

    Step_Third_Servlets_and_AOP (test): add PostgresTestContainer.java

---
+++ Step_Third_Servlets_and_AOP/src/test/java/me/oldboy/market/test_container/PostgresTestContainer.java
@@ -0,0 +1,29 @@
+package me.oldboy.market.test_container;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+@Testcontainers
+public abstract class PostgresTestContainer {
+    /* Делаем тестовый контейнер */
+    @Container
+    public static PostgreSQLContainer<?> postgresContainer =
+            new PostgreSQLContainer<>("postgres:14").withDatabaseName("test_db")
+                    .withUsername("test")
+                    .withPassword("test");
+
+    /* Настраиваем состояние контейнера и БД, до и после каждого / всех тестов */
+    @BeforeAll
+    public static void startTestContainer() {
+        postgresContainer.start();
+    }
+
+    @AfterAll
+    public static void stopTestContainer() {
+        postgresContainer.stop();
+    }
+
+}
