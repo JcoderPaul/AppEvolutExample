c3de3fca39781a747d07c437faf5937a2054bd62
Paul <100008373+JcoderPaul@users.noreply.github.com>
2026-01-20 07:37:35 +0500

    Step_Fourth_nonBoot_SpringApp: add Category.java

---
+++ Step_Fourth_nonBoot_SpringApp/src/main/java/me/oldboy/market/entity/prod_species/Category.java
@@ -0,0 +1,40 @@
+package me.oldboy.market.entity.prod_species;
+
+import jakarta.persistence.*;
+import lombok.*;
+import me.oldboy.market.entity.Product;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Класс, представляет категорию товара в каталоге.
+ * Содержит информацию об идентификаторе и названии категории.
+ */
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Getter
+@Setter
+@EqualsAndHashCode(exclude = "productList")
+@ToString(exclude = "productList")
+@Entity
+@Table(name = "categories", schema = "my_market")
+public class Category {
+    /**
+     * Уникальный идентификатор категории
+     */
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "category_id")
+    private Integer id;
+    /**
+     * Название категории
+     */
+    @Column(name = "category_name")
+    private String name;
+
+    @Builder.Default
+    @OneToMany(mappedBy = "category")
+    private List<Product> productList = new ArrayList<>();
+}
