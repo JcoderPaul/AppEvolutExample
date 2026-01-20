717161e49cad7c279baefce53c817156d02c5ca6
Paul <100008373+JcoderPaul@users.noreply.github.com>
2026-01-20 07:30:56 +0500

    Step_Fourth_nonBoot_SpringApp: add Loggable.java

---
+++ Step_Fourth_nonBoot_SpringApp/src/main/java/me/oldboy/market/aop/annotations/Loggable.java
@@ -0,0 +1,14 @@
+package me.oldboy.market.aop.annotations;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Аннотация помечающая методы и классы для проведения процедуры логирования (в нашем случае "замер скорости").
+ */
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.METHOD, ElementType.TYPE})
+public @interface Loggable {
+}
