2bda8b63b2a98127f5ea33df81a6d01958b596dd
Paul <100008373+JcoderPaul@users.noreply.github.com>
2026-01-20 07:35:08 +0500

    Step_Fourth_nonBoot_SpringApp: add AuditReadDto.java

---
+++ Step_Fourth_nonBoot_SpringApp/src/main/java/me/oldboy/market/dto/audit/AuditReadDto.java
@@ -0,0 +1,19 @@
+package me.oldboy.market.dto.audit;
+
+import lombok.Builder;
+import me.oldboy.market.entity.enums.Action;
+import me.oldboy.market.entity.enums.Status;
+
+import java.time.LocalDateTime;
+
+/**
+ * DTO для представления извлеченной из БД аудит-записи.
+ */
+@Builder
+public record AuditReadDto(Long id,
+                           LocalDateTime createAt,
+                           String createBy,
+                           Action action,
+                           Status isSuccess,
+                           String auditableRecord) {
+}
