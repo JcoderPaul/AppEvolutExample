5d8bb5016b99e775551c75f70683b28aa9c23dbe
Paul <100008373+JcoderPaul@users.noreply.github.com>
2026-01-20 07:41:24 +0500

    Step_Fourth_nonBoot_SpringApp: add UserRepository.java

---
+++ Step_Fourth_nonBoot_SpringApp/src/main/java/me/oldboy/market/repository/UserRepository.java
@@ -0,0 +1,38 @@
+package me.oldboy.market.repository;
+
+import me.oldboy.market.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+/**
+ * Репозиторий для работы с пользователями в БД.
+ * Предоставляет методы для выполнения CRUD операций и поиска пользователей.
+ * Наследует стандартные операции из {@link JpaRepository} и {@link CrudRepository}.
+ *
+ * @see User
+ * @see JpaRepository
+ * @see CrudRepository
+ */
+@Repository
+public interface UserRepository extends JpaRepository<User, Long>, CrudRepository<User, Long> {
+
+    /**
+     * Находит пользователя по идентификатору.
+     *
+     * @param userId идентификатор пользователя
+     * @return {@link Optional} с найденным пользователем или пустой {@link Optional}
+     */
+    Optional<User> findById(@Param("userId") Long userId);
+
+    /**
+     * Находит пользователя по email адресу.
+     *
+     * @param email email адрес пользователя
+     * @return {@link Optional} с найденным пользователем или пустой {@link Optional}
+     */
+    Optional<User> findByEmail(@Param("email") String email);
+}
