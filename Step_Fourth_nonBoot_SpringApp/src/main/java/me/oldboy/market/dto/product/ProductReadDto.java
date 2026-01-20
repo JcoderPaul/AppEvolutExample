b6931c8b5bc2baa8b52085bd2c29237e8702079d
Paul <100008373+JcoderPaul@users.noreply.github.com>
2026-01-20 07:36:09 +0500

    Step_Fourth_nonBoot_SpringApp: add ProductReadDto.java

---
+++ Step_Fourth_nonBoot_SpringApp/src/main/java/me/oldboy/market/dto/product/ProductReadDto.java
@@ -0,0 +1,21 @@
+package me.oldboy.market.dto.product;
+
+import lombok.Builder;
+
+import java.time.LocalDateTime;
+
+/**
+ * DTO для представления (чтения) товара.
+ */
+@Builder
+public record ProductReadDto(Long id,
+                             String name,
+                             double price,
+                             String categoryName,
+                             String brandName,
+                             String description,
+                             int stockQuantity,
+                             LocalDateTime creationAt,
+                             LocalDateTime modifiedAt) {
+}
+
