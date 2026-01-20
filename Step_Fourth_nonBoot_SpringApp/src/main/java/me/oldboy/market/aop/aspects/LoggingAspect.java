a561d36750a375d3c7adc6029f3da6ac17709be1
Paul <100008373+JcoderPaul@users.noreply.github.com>
2026-01-20 07:31:23 +0500

    Step_Fourth_nonBoot_SpringApp: add LoggingAspect.java

---
+++ Step_Fourth_nonBoot_SpringApp/src/main/java/me/oldboy/market/aop/aspects/LoggingAspect.java
@@ -0,0 +1,50 @@
+package me.oldboy.market.aop.aspects;
+
+import lombok.extern.slf4j.Slf4j;
+import me.oldboy.market.aop.annotations.Loggable;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.stereotype.Component;
+
+/**
+ * Аспектный класс для обработки методов помеченных аннотацией {@link Loggable}
+ */
+@Slf4j
+@Aspect
+@Component
+public class LoggingAspect {
+
+    /**
+     * Условия "врезки в метод" фиксирующие аннотированные {@link Loggable} методы
+     */
+    @Pointcut("@annotation(me.oldboy.market.aop.annotations.Loggable) && execution(* *(..))")
+    public void loggableMethods() {
+    }
+
+    /**
+     * Адвайс-метод запускающий логирование при старте метода, его финише и рассчитывающий время работы метода
+     *
+     * @param joinPoint "точка среза" перехватываемого метода.
+     * @return результат "взрезанного" метода
+     * @throws Throwable если возникает ошибка во время выполнения метода
+     */
+    @Around("loggableMethods()")
+    public Object loggableMethods(ProceedingJoinPoint joinPoint) throws Throwable {
+        String methodName = joinPoint.getSignature().toShortString();
+        /*
+        Можно использовать вариант конкатенации строк: log.info("Calling method (начало метода): " + methodName);
+        Но, в реальных приложениях для ускорения процесса рекомендуется применять вариант с аргументами см. ниже.
+        */
+        log.info("Calling method (начало метода): {}", methodName);
+
+        long startTime = System.currentTimeMillis();
+        Object result = joinPoint.proceed();
+        long endTime = System.currentTimeMillis();
+
+        log.info("Execution of method (обработка и завершение метода) {} finished. " +
+                "Execution time is (время работы метода в мс.) {} ms.", methodName, (endTime - startTime));
+        return result;
+    }
+}
