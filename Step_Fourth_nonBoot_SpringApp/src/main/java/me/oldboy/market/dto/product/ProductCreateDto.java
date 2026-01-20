58b95cfddbdc0e4cd60a62d60077800ade55fa9e
Paul <100008373+JcoderPaul@users.noreply.github.com>
2026-01-20 07:35:55 +0500

    Step_Fourth_nonBoot_SpringApp: add ProductCreateDto.java

---
+++ Step_Fourth_nonBoot_SpringApp/src/main/java/me/oldboy/market/dto/product/ProductCreateDto.java
@@ -0,0 +1,39 @@
+package me.oldboy.market.dto.product;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.*;
+import lombok.Builder;
+
+/**
+ * DTO для создания продукта (товара).
+ */
+@Builder
+public record ProductCreateDto(@NotEmpty(message = "Product name can not be EMPTY")
+                               @Size(min = 3, max = 256, message = "Wrong format (to short/to long)")
+                               @Schema(description = "Название товара (продукта)", example = "Tank T90")
+                               String name,
+
+                               @NotNull
+                               @Digits(integer = 22, fraction = 2)
+                               @Schema(description = "Цена товара (продукта)", example = "1500.55")
+                               Double price,
+
+                               @NotNull
+                               @Positive
+                               @Schema(description = "Идентификатор категории товара (продукта)", example = "1")
+                               Integer categoryId,
+
+                               @NotNull
+                               @Positive
+                               @Schema(description = "Идентификатор брэнда товара (продукта)", example = "1")
+                               Integer brandId,
+
+                               @NotEmpty(message = "Description can not be EMPTY")
+                               @Size(min = 3, max = 256, message = "Wrong format (to short/to long)")
+                               @Schema(description = "Описание товара (продукта)", example = "Танки грязи не боятся")
+                               String description,
+
+                               @NotNull
+                               @Schema(description = "Доступное количество товара (продукта)", example = "30")
+                               Integer stockQuantity) {
+}
