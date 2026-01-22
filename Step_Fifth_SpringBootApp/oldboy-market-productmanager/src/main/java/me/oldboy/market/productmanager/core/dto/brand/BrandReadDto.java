18291f5f1c4aba39e09edfffd5d98032cf8f2b90
Paul <100008373+JcoderPaul@users.noreply.github.com>
2026-01-22 09:50:30 +0500

    Step_Fifth_SpringBootApp (oldboy-market-productmanager): add BrandReadDto.java

---
+++ Step_Fifth_SpringBootApp/oldboy-market-productmanager/src/main/java/me/oldboy/market/productmanager/core/dto/brand/BrandReadDto.java
@@ -0,0 +1,9 @@
+package me.oldboy.market.productmanager.core.dto.brand;
+
+/**
+ * DTO для представления брэнда товара.
+ */
+public record BrandReadDto(Integer id,
+                           String name) {
+
+}
