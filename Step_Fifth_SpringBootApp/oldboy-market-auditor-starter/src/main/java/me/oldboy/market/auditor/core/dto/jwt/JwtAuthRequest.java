83a6d7be5480979f1ff74fea936b4b578d3cdd5e
Paul <100008373+JcoderPaul@users.noreply.github.com>
2026-01-22 09:42:02 +0500

    Step_Fifth_SpringBootApp (oldboy-market-auditor-starter): add JwtAuthRequest.java

---
+++ Step_Fifth_SpringBootApp/oldboy-market-auditor-starter/src/main/java/me/oldboy/market/auditor/core/dto/jwt/JwtAuthRequest.java
@@ -0,0 +1,35 @@
+package me.oldboy.market.auditor.core.dto.jwt;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * Объект передачи данных (DTO), представляющий информацию аутентификации.
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class JwtAuthRequest {
+
+    /**
+     * Email (логин) пользователя для аутентификации
+     */
+    @NotEmpty
+    @Size(min = 3, max = 64, message = "Wrong format (to short/to long)")
+    @Schema(description = "Email (as логин) зарегистрированный в 'системе'", example = "admin@admin.ru")
+    private String email;
+
+    /**
+     * Пароль пользователя для аутентификации
+     */
+    @NotEmpty
+    @Size(min = 3, max = 128, message = "Wrong format (to short/to long)")
+    @Schema(description = "Пароль для доступа 'в систему'", example = "1234")
+    private String password;
+}
