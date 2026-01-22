3e64987924027fde59fb6befe8c8e075d8f8850d
Paul <100008373+JcoderPaul@users.noreply.github.com>
2026-01-22 09:50:42 +0500

    Step_Fifth_SpringBootApp (oldboy-market-productmanager): add CategoryReadDto.java

---
+++ Step_Fifth_SpringBootApp/oldboy-market-productmanager/src/main/java/me/oldboy/market/productmanager/core/dto/category/CategoryReadDto.java
@@ -0,0 +1,8 @@
+package me.oldboy.market.productmanager.core.dto.category;
+
+/**
+ * DTO для представления категории товара.
+ */
+public record CategoryReadDto(Integer id,
+                              String name) {
+}
