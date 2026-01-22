f8ec2b5935680e43ef8934c471ff3692f57a6a90
Paul <100008373+JcoderPaul@users.noreply.github.com>
2026-01-22 10:02:06 +0500

    Step_Fifth_SpringBootApp: add SecurityUserDetails.java

---
+++ Step_Fifth_SpringBootApp/src/main/java/me/oldboy/market/config/security_details/SecurityUserDetails.java
@@ -0,0 +1,83 @@
+package me.oldboy.market.config.security_details;
+
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import me.oldboy.market.usermanager.core.entity.User;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+/**
+ * Класс определяющий UserDetails для Spring Security
+ */
+@NoArgsConstructor
+@AllArgsConstructor
+public class SecurityUserDetails implements UserDetails {
+
+    private User user;
+
+    /**
+     * Получить список текущих прав пользователей
+     *
+     * @return коллекция прав пользователей
+     */
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        List<GrantedAuthority> authorities = new ArrayList<>();
+        authorities.add(new SimpleGrantedAuthority(user.getRole().name()));
+        return authorities;
+    }
+
+    /**
+     * Получить логин или имя текущего пользователя
+     *
+     * @return имя пользователя (логин)
+     */
+    @Override
+    public String getUsername() {
+        return user.getEmail();
+    }
+
+    /**
+     * Получить текущий пароль пользователя
+     *
+     * @return пароль пользователя
+     */
+    @Override
+    public String getPassword() {
+        return user.getPassword();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+    /**
+     * Получить текущего пользователя
+     *
+     * @return сущность пользователя
+     */
+    public User getUser() {
+        return this.user;
+    }
+}
