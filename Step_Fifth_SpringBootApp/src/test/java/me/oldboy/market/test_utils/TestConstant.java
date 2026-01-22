2ceb1cc657f857ca6d22d6b2a12fce46d5c65e97
Paul <100008373+JcoderPaul@users.noreply.github.com>
2026-01-22 10:12:14 +0500

    Step_Fifth_SpringBootApp (test): add TestConstant.java

---
+++ Step_Fifth_SpringBootApp/src/test/java/me/oldboy/market/test_utils/TestConstant.java
@@ -0,0 +1,9 @@
+package me.oldboy.market.test_utils;
+
+public class TestConstant {
+    public static final Long existId = 1L;
+    public static final Long nonExistId = 100L;
+    public static final String existUserEmail = "admin@admin.ru";
+    public static final String nonExistUserEmail = "sanara@swordwing.de";
+    public static final String notValidToken = "not_valid_token";
+}
